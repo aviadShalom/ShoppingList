708cc2a8c551013e9190e3b533f3c315515a1ee5
Aviad <aviad1015@gmail.com>
2019-06-25 16:22:14 +0300

    add json to response

--- Server/app.py
+++ Server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def home():
 
 @app.route('/GetShoppingList', methods=['POST'])
 def get_shopping_list():
-    return "Shopping list"
+    return jsonify({'app':'sssss'})
 
 if __name__ == "__main__":
     app.run(port=5000)
